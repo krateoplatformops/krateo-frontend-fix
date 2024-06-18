8cb9a3ca8641eaf7d9203b2bc5ba4bd43bde2343
codev99 <58194031+codev99@users.noreply.github.com>
2024-06-18 20:53:45 +0200

    fix: parsing elements
    * feat: 74 custom deployments
    
    * feat: new widget parser
    
    * feat: 74 custom deployment section
    
    * feat: 74 hide dashboard menu item
    
    * feat: 74 kind and apiVersion on form submit
    
    * fix: 74 parsing elements

--- src/hooks/useParseData.tsx
+++ src/hooks/useParseData.tsx
@@ -21,21 +21,21 @@ const useParseData = () => {
     const renderComponent = (data, index) => {
       switch (data?.type) {
         case "row":
-          return <Row key={`row_${data.uid}`} className={styles.row}>{ data.items.map(item => parseContent(item, index+1)) }</Row>
+          return <Row key={`row_${data.uid}`} className={styles.row}>{ data.items?.map(item => parseContent(item, index+1)) }</Row>
         case "column":
-          return <Col key={`column_${data.uid}`} { ...getColProps(data.props.width) } className={styles.col}>{ data.items.map(item => parseContent(item, index+1)) }</Col>
+          return <Col key={`column_${data.uid}`} { ...getColProps(data.props.width) } className={styles.col}>{ data.items?.map(item => parseContent(item, index+1)) }</Col>
         case "tablist":
-          return <Tabs key={data.uid} className={styles.tabs}>{ data.items.map(item => parseContent(item, index+1)) }</Tabs>
+          return <Tabs key={data.uid} className={styles.tabs}>{ data.items?.map(item => parseContent(item, index+1)) }</Tabs>
         case "tabpane":
-          return <TabPane key={data.uid} tab={data.props.label} className={styles.tabpane}>{ data.items.map(item => parseContent(item, index+1)) }</TabPane>
+          return <TabPane key={data.uid} tab={data.props.label} className={styles.tabpane}>{ data.items?.map(item => parseContent(item, index+1)) }</TabPane>
         case "panel":
-          return <Panel key={data.uid} title={undefined} tooltip={undefined} buttons={undefined} content={data.items.map(item => parseContent(item, index+1))} />
+          return <Panel key={data.uid} title={undefined} tooltip={undefined} buttons={undefined} content={data.items?.map(item => parseContent(item, index+1))} />
         case "Toolbar": //TODO
           return <Toolbar key={data.uid}>{ parseContent(data.status.content.items, index+1) }</Toolbar>
         default:
           if (data?.type) {
             const Component = widgets[data.type];
-            return data.items.map((el, i) => <Component id={`${data.uid}_${i}`} key={`widget_${data.uid}_${i}`} actions={el.actions} {...{...el.app, ...data.props}} />)
+            return data.items?.map((el, i) => <Component id={`${data.uid}_${i}`} key={`widget_${data.uid}_${i}`} actions={el.actions} {...{...el.app, ...data.props}} />)
           } else {
             // null -> exit recoursive loop
             return <></>
